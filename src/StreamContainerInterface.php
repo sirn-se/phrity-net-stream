0e890a294b063e82a9d7503759c184edf1482193
Sören Jensen <sirn@sirn.se>
2026-01-05 15:31:58 +0100

    Identity in StreamContainer

--- src/StreamContainerInterface.php
+++ src/StreamContainerInterface.php
@@ -11,4 +11,9 @@ interface StreamContainerInterface
      * @return StreamInterface
      */
     public function getStream(): StreamInterface;
+
+    /**
+     * @return non-empty-string
+     */
+    public function getIdentity(): string;
 }
